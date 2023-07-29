36248cde1e1783f1acfcddb5f9bef57867753c6b
Christian Schrader <christian.s1989@gmail.com>
2023-07-29 23:51:10 +0200

    inherit deviceclass from BaseDeviceClass

--- enocean_ble/parser.py
+++ enocean_ble/parser.py
@@ -4,7 +4,7 @@ from typing import Optional
 from bluetooth_data_tools import short_address
 from bluetooth_sensor_state_data import BluetoothData
 from home_assistant_bluetooth import BluetoothServiceInfo
-from sensor_state_data import BinarySensorDeviceClass
+from sensor_state_data import BaseDeviceClass
 
 from .decoder import PTM215BDecoder, TelegramType
 
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 MANUFACTURER_ID = 0x3DA
 
-class EnoceanDeviceClass(BinarySensorDeviceClass):
+class EnoceanBinaryDeviceClass(BaseDeviceClass):
     # On means button pressed
     PRESSED = "pressed"
 
@@ -54,25 +54,25 @@ class EnoceanBluetoothDeviceData(BluetoothData):
         self.update_binary_sensor(
             f"a0_pressed",
             decoder.a0_action and decoder.is_press_action,
-            EnoceanDeviceClass.PRESSED,
+            EnoceanBinaryDeviceClass.PRESSED,
             "Channel A0",
         )
         self.update_binary_sensor(
             "a1_pressed",
             decoder.a1_action and decoder.is_press_action,
-            EnoceanDeviceClass.PRESSED,
+            EnoceanBinaryDeviceClass.PRESSED,
             "Channel A1",
         )
         self.update_binary_sensor(
             "b0_pressed",
             decoder.b0_action and decoder.is_press_action,
-            EnoceanDeviceClass.PRESSED,
+            EnoceanBinaryDeviceClass.PRESSED,
             "Channel B0",
         )
         self.update_binary_sensor(
             "b1_pressed",
             decoder.b1_action and decoder.is_press_action,
-            EnoceanDeviceClass.PRESSED,
+            EnoceanBinaryDeviceClass.PRESSED,
             "Channel B1",
         )
 
